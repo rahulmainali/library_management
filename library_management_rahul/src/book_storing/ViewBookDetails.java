a4c3d58d425eb6d72b82b6ab7c7656b446821744
rahulmainali <50787264+rahulmainali@users.noreply.github.com>
2021-09-27 12:23:40 +0545

    Book Detaills updated

--- library_management_rahul/src/book_storing/ViewBookDetails.java
+++ library_management_rahul/src/book_storing/ViewBookDetails.java
@@ -135,3 +135,138 @@ public class ViewBookDetails {
 				JTextField tfDate = new JTextField();
 				f.add(tfDate);
 				tfDate.setBounds(880, 510, 200, 30);
+				
+				JLabel lStatus = new JLabel("Status");
+				f.add(lStatus);
+				lStatus.setBounds(280, 550, 200, 50);
+				String s[]= {"Available", "Out of Stock"};
+				JComboBox<String> c1= new JComboBox(s);
+				f.add(c1);
+				c1.setBounds(340, 560, 150, 30);
+
+				JLabel lPrice = new JLabel("Price");
+				f.add(lPrice);
+				lPrice.setBounds(540, 550, 200, 50);
+				JTextField tfPrice = new JTextField();
+				f.add(tfPrice);
+				tfPrice.setBounds(600, 560, 150, 30);
+
+				JButton btnChange = new JButton("Make Change");
+				f.add(btnChange);
+				btnChange.setBounds(400, 600, 150, 45);
+
+				JButton btnCancle = new JButton("Cancel");
+				f.add(btnCancle);
+				btnCancle.setBounds(580, 600, 150, 45);
+
+				TableModel model = jtBook.getModel();
+				int ID = (int) model.getValueAt(row, 0);
+				tfId.setText(ID + "");
+
+				tfName.setText((String) model.getValueAt(row, 1));
+				tfPName.setText((String) model.getValueAt(row, 2));
+				tfDate.setText((String) model.getValueAt(row, 3));
+				c1.setSelectedItem((String) model.getValueAt(row, 4));
+				int Price = (int) model.getValueAt(row, 5);
+				tfPrice.setText(Price + "");
+
+				// update action
+
+				btnChange.addActionListener(e3 -> {
+					int ID1 = Integer.parseInt(tfId.getText());
+					String Name = tfName.getText();
+					String PublisherName = tfPName.getText();
+					String PublishedDate = tfDate.getText();
+					String Status1 = (String) c1.getSelectedItem();
+					int Price1 = Integer.parseInt(tfPrice.getText());
+					int ID2 = (int) model.getValueAt(row, 0);
+					String query1 = "Update book_table set Name='" + Name + "',PublisherName='" + PublisherName
+							+ "',PublishedDate='" + PublishedDate + "',Status='" + Status1 + "', Price='" + Price
+							+ "' where Id='" + ID1 + "'";
+
+					System.out.println(query1);
+
+					System.out.println(ID1 + Name + PublisherName + PublishedDate + Status1 + Price1);
+
+					try {
+						int upresult = db.connection().executeUpdate(query1);
+						System.out.println(upresult);
+
+						if (upresult > 0) {
+
+							JOptionPane.showMessageDialog(sp, "Book updated");
+							new ViewBookDetails();
+							f.dispose();
+						}
+
+					} catch (SQLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				});
+
+				// cancle action
+				btnCancle.addActionListener(e2 -> {
+					new ViewBookDetails();
+					f.dispose();
+				});
+
+			} else {
+
+				JOptionPane.showMessageDialog(sp, "Select Row for Update");
+			}
+		});
+		
+		JLabel lSearch, lSort;
+		JTextField tfSearch;
+		JTextField tfSearch1;
+		JComboBox tfSort;
+		JTextField tfSort1;
+		
+		JButton btnSearch, btnSort;
+		
+		lSearch= new JLabel("Search By");
+		f.add(lSearch);
+		lSearch.setBounds(890, 30, 250, 50);
+		lSearch.setFont(new Font("Serif", Font.CENTER_BASELINE,20));
+		
+		tfSearch = new JTextField(30);
+		f.add(tfSearch);
+		tfSearch.setBounds(880, 80, 120, 25);
+		
+		
+		
+		btnSearch = new JButton("Search");
+		f.add(btnSearch);
+		btnSearch.setBounds(950, 150, 100, 30);
+
+
+		
+//		lSort= new JLabel("Sort In");
+//		f.add(lSort);
+//		lSort.setBounds(1030, 30, 250, 50);
+//		lSort.setFont(new Font("Serif", Font.CENTER_BASELINE,20));
+//		 
+//		String s2[]= {"Ascending","Descending"};
+//		tfSort= new JComboBox<>(s2);
+//		f.add(tfSort);
+//		tfSort.setBounds(1020, 80, 120, 25);
+//		tfSort1= new JTextField(50);
+//		f.add(tfSort1);
+//		tfSort1.setBounds(1020, 120, 120, 25);
+
+		
+//		btnSort = new JButton("Sort");
+//		f.add(btnSort);
+//		btnSort.setBounds(1030, 200, 100, 25);
+//		btnSort.setFont(new Font("Serif", Font.CENTER_BASELINE,20));
+//		
+
+		f.setSize(1200, 800);
+		f.setLayout(null);
+		f.setVisible(true);
+		f.setLocationRelativeTo(null);
+		f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+	}
+
+}
