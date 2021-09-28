7b309ddda676f1756a01f383353e17da1ccfcad6
rahulmainali <50787264+rahulmainali@users.noreply.github.com>
2021-09-28 19:51:53 +0545

    Database connection

---
+++ library_management_rahul/src/book_storing/DbConnect.java
@@ -0,0 +1,23 @@
+package book_storing;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DbConnect {
+	public Statement connection() {
+		Connection con;
+		Statement stmt= null;
+		try {
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/dbms","root","root");
+			stmt = con.createStatement();
+		} catch (SQLException e1) {
+			
+			e1.printStackTrace();
+		}
+		return stmt;
+	}
+
+}
+
