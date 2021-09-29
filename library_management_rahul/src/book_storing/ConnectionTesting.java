c181587b33341d16a181bb3d09b7d766b83b1b8d
rahulmainali <50787264+rahulmainali@users.noreply.github.com>
2021-09-29 19:29:27 +0545

    Create ConnectionTesting.java

---
+++ library_management_rahul/src/book_storing/ConnectionTesting.java
@@ -0,0 +1,23 @@
+package book_storing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.Statement;
+
+import org.junit.jupiter.api.Test;
+
+
+
+class ConnectionTesting {
+
+	@Test
+	void test() {
+		
+		DbConnect db= new DbConnect(); //making an object db
+		Statement output = (Statement) db.connection();
+		Object expectedOutput = null;
+		assertEquals(expectedOutput,output);
+		
+	}
+
+}
